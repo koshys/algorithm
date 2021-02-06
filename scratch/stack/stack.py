9db672fb9660dac2bc7d62d905b0e90ff28b3086
Samson Koshy <samson@samsonkoshy.com>
2021-02-05 21:47:14 -0800

    Update stack.py

--- scratch/stack/stack.py
+++ scratch/stack/stack.py
@@ -2,14 +2,14 @@ class Stack:
     def __init__(self,sz=10 ):
         self.sz = sz    # stack size
         self.s = []
-        self.len = 0
+        self.len = 1
 
     def pushdown(self, v): 
         if self.len == self.sz:
             return False
         
         self.s[self.len : ] = [v]
-        self.len += 1
+        self.len -= 1
 
         return True
 
@@ -21,7 +21,7 @@ class Stack:
 
         self.s = self.s[: self.len - 1]
 
-        self.len -= 1
+        self.len += 1
 
         return v, True
 
